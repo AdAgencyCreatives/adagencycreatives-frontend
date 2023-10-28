5553f4c710afc3e82dc789abcd7370e76ecb3f35
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2023-10-28 13:57:55 +0500

    my changes

---
+++ src/components/NumUnit.js
@@ -0,0 +1,21 @@
+const NumUnit = (props) => {
+
+    const getNumUnit = (number) => {
+        if(number <= 0) {
+            return '0'; 
+        }
+        
+        const k = 1000
+        const units = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
+
+        const i = Math.floor(Math.log(number) / Math.log(k))
+
+        return `${parseFloat((number / Math.pow(k, i)).toFixed(0))} ${units[i]}`
+    }
+    
+    return (
+        <span>{getNumUnit(props.number || 0)}</span>
+    );
+};
+
+export default NumUnit;
