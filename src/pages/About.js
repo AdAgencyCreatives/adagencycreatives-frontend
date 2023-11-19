6d9e695ea7aaa05152ff61d4df6ac550090c4ac7
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2023-11-20 01:29:25 +0500

    About us and ticker updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,37 +1,26 @@
 const About = () => {
   return (
     <>
-    <div className="bg-black p-4"></div>
+      <div className="bg-black p-4"></div>
       <div className="container mt-5">
         <h3 className="text-dark text-center fw-normal">
-          Ad Agency Creatives come together
-          <br />
-          <br />
-          talk about the industry
-          <br />
-          <br />
-          talk about the work
-          <br />
-          <br />
-          meet other creatives
-          <br />
-          <br />
-          share ideas and resources
-          <br />
-          <br />
-          identify and manage job opportunities
-          <br />
-          <br />
-          do really do cool $#*t!
-          <br />
-          <br />
+          <span style={{ "font-size":"36px", "font-weight":"500" }}>About Us</span><br /><br />
+          Ad Agency Creatives is a community for Creatives<br /><br />
+          to come together<br /><br />
+          talk about the industry<br /><br />
+          talk about the work<br /><br />
+          meet other creatives<br /><br />
+          share ideas and resources<br /><br />
+          manage job opportunities<br /><br />
+          mentor and be mentored<br /><br />
+          and do really cool $#**!<br /><br />
         </h3>
       </div>
 
-      <div className="bg-black text-white text-center p-5 mb-3">
+      {/* <div className="bg-black text-white text-center p-5 mb-3">
         <h3 className="fs-2 fw-normal">We are bringing our industry together</h3>
         <p className="fs-5">to Gather. to Inspire. to do really cool $#*t!</p>
-      </div>
+      </div> */}
     </>
   );
 };
