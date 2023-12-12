114df88a0aa64cff774d4084211fe8f22bdd351b
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2023-12-12 23:25:58 +0500

    Creative - MyResume  - 5 samples - set not required.

--- src/pages/User/Creative/MyResume.js
+++ src/pages/User/Creative/MyResume.js
@@ -652,7 +652,7 @@ const MyResume = () => {
   const [portfolio, setPortfolio] = useState([
     {
       label: "Upload up to 5 samples of your best work. A sneak peek.",
-      required: true,
+      required: false,
       type: "upload",
       name: "portfolio_item",
       items: [],
