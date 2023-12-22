7091c4f838c7818bbb3e0f2b7bcf5a5b530c34e8
Asif Shabir <rao311883@gmail.com>
2023-12-23 00:25:48 +0500

    page data

--- src/context/PagesDataContext.js
+++ src/context/PagesDataContext.js
@@ -3,6 +3,7 @@ import { api } from "../api/api";
 export const getPageData = async (page) => {
     try {
         const response = await api.get("/pages?filter[page]=" + page);
+        localStorage.setItem(page, JSON.stringify(response.data));
         return response.data;
     } catch (error) {
     }
