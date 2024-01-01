aac905db66451ace6b6481e1dacee55ed48b51e9
Asif Shabir <rao311883@gmail.com>
2024-01-02 00:16:18 +0500

    Implement Featured Cities on Frontend

--- src/components/home/FeaturedCities.js
+++ src/components/home/FeaturedCities.js
@@ -1,28 +1,22 @@
-import Newyork from "../../assets/images/New-York.png";
-import Miami from "../../assets/images/Miami.png";
-import LosAngeles from "../../assets/images/Los-Angeles-1.png";
-import Dallas from "../../assets/images/Dallas-e1681932281853.png";
-import Chicago from "../../assets/images/Chicago-Nights-1-1.png";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { useContext, useEffect } from "react";
 import { Context } from "../../context/DataContext";
+import { useContext, useEffect } from "react";
 
 const FeaturedCities = () => {
 
-  const {state:{featured_cities},getFeaturedCities} = useContext(Context);
+  const {
+    state:{ featured_cities }, 
+    getFeaturedCities
+  } = useContext(Context);
 
   useEffect(() => {
     getFeaturedCities();
-  },[])
+  }, []);
 
-  const getJobCount = (name) => {
-    if(featured_cities.length){
-      const city = featured_cities.find((item) => item.name == name);
-      return city.count
-    }
-    return 0
-  }
+  useEffect(() => {
+    console.log(featured_cities);
+  }, [featured_cities]);
 
   return (
     <div id="cities">
@@ -37,95 +31,60 @@ const FeaturedCities = () => {
 
       <div className="sectionContent featured-cities">
         <div className="row gx-3">
-          <div className="col-12 col-md-4">
-            <div className="job-city-banner ">
-              <Link to={`/job-location/dallas`}>
-                <div className="city-banner-inner">
-                  <div
-                    className="bg-banner"
-                    style={{ backgroundImage: `url(${Dallas})` }}
-                  ></div>
-                  <div className="inner">
-                    <h4 className="title">Dallas </h4>
+          {featured_cities.map((item, index) => {
+            if (index > 0) {
+              return <></>;
+            }
+            return (
+              <div className="col-12 col-md-4">
+                <div className={`job-city-banner`}>
+                  <Link to={`/job-location/${item.slug}`}>
+                    <div className="city-banner-inner">
+                      <div
+                        className="bg-banner"
+                        style={{ backgroundImage: `url(${item.preview_link})` }}
+                      ></div>
+                      <div className="inner">
+                        <h4 className="title">{item.name}</h4>
 
-                    <div className="number">
-                      <span>{getJobCount("Dallas")}</span> jobs
+                        <div className="number">
+                          <span>{item.count}</span> jobs
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            )
+          })}
+          <div className="col-12 col-md-8">
+            <div className="row">
+              {featured_cities.map((item, index) => {
+                if (index === 0) {
+                  return <></>;
+                }
+                return (
+                  <div className="col-12 col-md-6">
+                    <div className={`job-city-banner job-banner-small`}>
+                      <Link to={`/job-location/${item.slug}`}>
+                        <div className="city-banner-inner">
+                          <div
+                            className="bg-banner"
+                            style={{ backgroundImage: `url(${item.preview_link})` }}
+                          ></div>
+                          <div className="inner">
+                            <h4 className="title">{item.name}</h4>
+
+                            <div className="number">
+                              <span>{item.count}</span> jobs
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
                     </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="col-12 col-md-4">
-            <div className="job-city-banner job-banner-small  ">
-              <Link to={`/job-location/los-angeles`}>
-                <div className="city-banner-inner">
-                  <div
-                    className="bg-banner"
-                    style={{ backgroundImage: `url(${LosAngeles})` }}
-                  ></div>
-                  <div className="inner">
-                    <h4 className="title">Los Angeles </h4>
-
-                    <div className="number">
-                      <span>{getJobCount("Los Angeles")}</span> jobs
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
-            <div className="job-city-banner job-banner-small ">
-              <Link to={`/job-location/new-york`}>
-                <div className="city-banner-inner">
-                  <div
-                    className="bg-banner"
-                    style={{ backgroundImage: `url(${Newyork})` }}
-                  ></div>
-                  <div className="inner">
-                    <h4 className="title">New York </h4>
-
-                    <div className="number">
-                      <span>{getJobCount("New York")}</span> jobs
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="col-12 col-md-4">
-            <div className="job-city-banner job-banner-small ">
-              <Link to={`/job-location/chicago`}>
-                <div className="city-banner-inner">
-                  <div
-                    className="bg-banner"
-                    style={{ backgroundImage: `url(${Chicago})` }}
-                  ></div>
-                  <div className="inner">
-                    <h4 className="title">Chicago </h4>
-
-                    <div className="number">
-                      <span>{getJobCount("Chicago")}</span> jobs
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
-            <div className="job-city-banner job-banner-small ">
-              <Link to={`/job-location/miami`}>
-                <div className="city-banner-inner">
-                  <div
-                    className="bg-banner"
-                    style={{ backgroundImage: `url(${Miami})` }}
-                  ></div>
-                  <div className="inner">
-                    <h4 className="title">Miami </h4>
-
-                    <div className="number">
-                      <span>{getJobCount("Miami")}</span> jobs
-                    </div>
-                  </div>
-                </div>
-              </Link>
+                )
+              })}
             </div>
           </div>
         </div>
