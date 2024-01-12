01eeade519c45a780dcfa6f98000713b943e9dfc
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-01-12 07:25:17 +0500

    advisor jobs update

--- src/pages/User/Agency/MyJobs.js
+++ src/pages/User/Agency/MyJobs.js
@@ -43,7 +43,9 @@ const MyJobs = () => {
             <table className="job-table">
               <thead>
                 <tr>
-                  <th className="title">Agency</th>
+                  {user?.role == 'advisor' && (
+                    <th className="title">Agency</th>
+                  )}
                   <th className="title">Title</th>
                   <th className="applicants">Applicants</th>
                   <th className="date">Created &amp; Expired</th>
