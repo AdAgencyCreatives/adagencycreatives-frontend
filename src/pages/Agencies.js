4243e0e9884cb3a6c17df00a8014dc2dc2d3159f
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-02-09 00:11:57 +0500

    shortlist button fix

--- src/pages/Agencies.js
+++ src/pages/Agencies.js
@@ -62,7 +62,7 @@ const Agencies = () => {
                     {(role == "admin" || role == "creative") && (
                       <Tooltip title={"Shortlist"} type="featured">
                         <button className={"shortlist-btn" + (isShortlisted ? " active" : "")} onClick={() => (isShortlisted ? removeBookmark(isShortlisted.id) : addToShortlist(item.id))}>
-                          <IoBookmarkOutline />
+                          <IoBookmarkOutline size={24} />
                         </button>
                       </Tooltip>
                     )}
