2fff67ccace50a8654a9c639262709081b76e317
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-02-15 00:06:03 +0500

    Jobs Pagination Fix

--- src/pages/Jobs/Jobs.js
+++ src/pages/Jobs/Jobs.js
@@ -406,19 +406,19 @@ const Jobs = () => {
                         <nav>
                           <ul className="pagination">
                             <li className={"page-item" + (meta.current_page == 1 ? " disabled" : "")} onClick={() => paginate(meta.current_page - 1)}>
-                              <a className="page-link" href="#" aria-label="Previous">
+                              <a className="page-link" href="javascript:void(0);" aria-label="Previous">
                                 <span aria-hidden="true">&laquo;</span>
                               </a>
                             </li>
                             {Array.apply(null, { length: meta.last_page }).map((item, index) => (
                               <li className={"page-item " + (meta.current_page == index + 1 ? "active" : "")} onClick={() => paginate(index + 1)} key={"page" + index}>
-                                <a className="page-link" href="#">
+                                <a className="page-link" href="javascript:void(0);">
                                   {index + 1}
                                 </a>
                               </li>
                             ))}
                             <li className={"page-item" + (meta.current_page == meta.last_page ? " disabled" : "")} onClick={() => paginate(meta.current_page + 1)}>
-                              <a className="page-link" href="#" aria-label="Next">
+                              <a className="page-link" href="javascript:void(0);" aria-label="Next">
                                 <span aria-hidden="true">&raquo;</span>
                               </a>
                             </li>
