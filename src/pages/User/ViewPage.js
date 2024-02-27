00484d363875526e65cd83807b8cf99f84bc49ec
Asif Shabir <rao311883@gmail.com>
2024-02-27 20:55:53 +0500

    other fixes

--- src/pages/User/ViewPage.js
+++ src/pages/User/ViewPage.js
@@ -9,18 +9,6 @@ const ViewPage = () => {
     const { username, type, role_name } = useParams();
     const page = type;
 
-    const [isLoading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
-    }, []);
-
-    if (isLoading) {
-        return <Loader />;
-    }
-
     return <>
         {(page == "creative" || page == "agency" || page == "advisor" || page == "recruiter") ? (
             <ViewProfile />
