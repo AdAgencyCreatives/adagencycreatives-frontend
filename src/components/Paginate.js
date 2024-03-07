2cd48c5e12472422aa1fe02fa823979467508106
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-03-07 15:10:20 +0500

    Paginate Fix

--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -8,20 +8,20 @@ const Paginate = ({ meta, paginate }) => {
                 <div className="user-pagination">
                     <nav>
                         <ul className="pagination">
-                            <li className={"page-item" + (meta.current_page == 1 ? " disabled" : "")} onClick={() => paginate(meta.current_page - 1)}>
-                                <a className="page-link" href="#" aria-label="Previous">
+                            <li className={"page-item" + (meta.current_page == 1 ? " disabled" : "")} onClick={() => (meta.current_page == 1 ? void (0) : paginate(meta.current_page - 1))}>
+                                <a className="page-link" href="javascript:void(0)" aria-label="Previous">
                                     <span aria-hidden="true">&laquo;</span>
                                 </a>
                             </li>
                             {Array.apply(null, { length: meta.last_page }).map((item, index) => (
                                 <li className={"page-item " + (meta.current_page == index + 1 ? "active" : "")} onClick={() => paginate(index + 1)} key={"page" + index}>
-                                    <a className="page-link" href="#">
+                                    <a className="page-link" href="javascript:void(0)" aria-label={"Page " + (index + 1)}>
                                         {index + 1}
                                     </a>
                                 </li>
                             ))}
-                            <li className={"page-item" + (meta.current_page == meta.last_page ? " disabled" : "")} onClick={() => paginate(meta.current_page + 1)}>
-                                <a className="page-link" href="#" aria-label="Next">
+                            <li className={"page-item" + (meta.current_page == meta.last_page ? " disabled" : "")} onClick={() => (meta.current_page == meta.last_page ? void (0) : paginate(meta.current_page + 1))}>
+                                <a className="page-link" href="javascript:void(0)" aria-label="Next">
                                     <span aria-hidden="true">&raquo;</span>
                                 </a>
                             </li>
