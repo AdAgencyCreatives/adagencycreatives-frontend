02465296314f89dd68ff28ff7dc32f5b290edc8a
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-08-06 07:47:03 +0500

    fix

--- src/pages/AgencySearch.js
+++ src/pages/AgencySearch.js
@@ -71,7 +71,7 @@ const AgencySearch = () => {
   }, [field, search]);
 
   return (
-    <div className="dark-container mb-0">
+    <div className="agencies-page dark-container mb-0">
       <div className="container p-md-0 px-5">
         <DelayedOutput>
           <h1 className="community-title text-white text-center mb-4">{search} Agencies</h1>
