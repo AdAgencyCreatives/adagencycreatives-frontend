52134b1578e9aa9a9e111d7422e7388e49ba9fb4
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-09-28 01:11:58 +0500

    fix

--- src/components/RestrictedAccess.js
+++ src/components/RestrictedAccess.js
@@ -16,7 +16,20 @@ const RestrictedAccess = ({ title = "Restricted Access", message = "Login to vie
     }, []);
 
     return timedLoading ? (
-        <CircularProgress />
+        <div className="dark-container page-community mb-0 mt-4">
+            <h1 className="community-title"></h1>
+            <div className="container-fluid mt-4">
+                <div className="row">
+                    <div className="col-md-12 mb-4 mb-md-0">
+                        <div className="restricted-creatives-only">
+                            <div className="restricted-message">
+                                <CircularProgress />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     ) : (
         <div className="dark-container page-community mb-0 mt-4">
             <h1 className="community-title">{title}</h1>
