71affbbf953e188d4b3d1b8a5fa994ab1ec1bde2
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-11-13 00:55:39 +0500

    fix

--- src/components/RouterErrorBoundary.js
+++ src/components/RouterErrorBoundary.js
@@ -15,16 +15,18 @@ const RouterErrorBoundary = () => {
             </Link>
             <div className='message' role="alert">
                 <h3 style={{ textAlign: 'center' }}>It seems something is not right.</h3>
-                <div style={{ textAlign: 'center' }}>
+                <h4 style={{ textAlign: 'center' }}>We are working on it.</h4>
+                {/* <div style={{ textAlign: 'center' }}>
                     We are working on it, but if you keep getting this screen for long,
                     you may want to reach us: <a href='mailto:membersupport@adagencycreatives.com'>membersupport@adagencycreatives.com</a>.
-                </div>
+                </div> */}
                 <div style={{ display: 'none' }}>{error.statusText || error.message}</div>
             </div>
             <img src={WeWillBeRightBack} />
             <div className='buttons'>
-                <button className="btn btn-dark hover-gold" onClick={(e) => window.location = '/'}>Home</button>
+                <Link className='btn btn-dark hover-gold' to={"/"}>Home</Link>
                 <button className="btn btn-dark hover-gold" onClick={(e) => window.location.reload()}>Reload</button>
+                <Link className='btn btn-dark hover-gold' to={"/contact"}>Contact Us</Link>
             </div>
         </div>
     );
