19a3eeb0e4b479193e769612ceeebcf1b0dc67b7
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-01-05 00:28:08 +0500

    fix

--- src/components/home/SpotlightReelSingle.js
+++ src/components/home/SpotlightReelSingle.js
@@ -22,27 +22,31 @@ const SpotlightReelSingle = ({ item, index }) => {
 
     return (<>
         <Link to={item.url} className="spotlight-reel" key={`m_${index}`} style={{ fontSize: '16px', flexDirection: 'column' }}
-            onClick={(e) => {
-                if (!showVideo) {
-                    setShowVideo(true);
-                    e.preventDefault();
-                    e.stopPropagation();
-                    return false;
-                }
-                return true;
-            }}>
+        // onClick={(e) => {
+        //     if (!showVideo) {
+        //         setShowVideo(true);
+        //         e.preventDefault();
+        //         e.stopPropagation();
+        //         return false;
+        //     }
+        //     return true;
+        // }}
+        // onMouseLeave={(e) => setShowVideo(false)}
+        >
             <video autoPlay muted style={{ display: showVideo ? 'block' : 'none' }}>
                 <source src={item.url} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
-            {!showVideo && (<>
-                <span className="view">View &gt;</span>
-                <span className="flex-centered intro">Introducing</span>
-                <span className="flex-centered title">{data[0]}</span>
-                <span className="flex-centered category">{data[1]}</span>
-                <img src={AdAgency} height={150} width={150} alt="" />
-            </>)}
-        </Link>
+            {
+                !showVideo && (<>
+                    <span className="view">View &gt;</span>
+                    <span className="flex-centered intro">Introducing</span>
+                    <span className="flex-centered title">{data[0]}</span>
+                    <span className="flex-centered category">{data[1]}</span>
+                    <img src={AdAgency} height={150} width={150} alt="" />
+                </>)
+            }
+        </Link >
     </>);
 };
 
