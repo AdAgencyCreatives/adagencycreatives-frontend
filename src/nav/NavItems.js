50554fb6f0af647d2b3461202a77708a7bfd6f0b
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-01-25 18:40:18 +0500

    fix

--- src/nav/NavItems.js
+++ src/nav/NavItems.js
@@ -22,6 +22,7 @@ export const navItems = [
       { name: "Feedback", hashLink: "#feedback", link: "/contact" },
     ],
   },
+  { name: "About", link: "/about", mobile: true },
   { name: "Jobs", link: "/creative-jobs", mobile: true },
   { name: "Creatives", link: "/creatives", auth: true },
   { name: "The Lounge", link: "/community", roles: ["admin", "creative"], restrictedMessage: 'Please login as a Creative to access' },
